74bc408d729d6b8d3db32efa00eef9aaa4aee255
Yan MATAGNE <ymatagne@norsys.fr>
2017-08-31 09:23:23 -0400

    feat(generator) : Link api with generator

--- src/main/java/com/github/pushmyshop/pushmyshopapi/handlers/CustomerEventHandler.java
+++ src/main/java/com/github/pushmyshop/pushmyshopapi/handlers/CustomerEventHandler.java
@@ -2,15 +2,11 @@ package com.github.pushmyshop.pushmyshopapi.handlers;
 
 import com.github.pushmyshop.pushmyshopapi.models.Customer;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
-import org.springframework.data.rest.core.event.AbstractRepositoryEventListener;
 import org.springframework.stereotype.Component;
 
-import java.util.logging.Logger;
+import java.io.IOException;
 
 @Component
 @Slf4j
@@ -20,6 +16,11 @@ public class CustomerEventHandler {
     @HandleAfterCreate
     public void handleAfterCreate(Customer customer) {
         log.info("[After Create] Create file for generate web app");
-        
+        ProcessBuilder process = new ProcessBuilder("yo", "pushmyshop", customer.getNameOfCompagny(), "/tmp/");
+        try {
+            process.start();
+        } catch (IOException exception) {
+            log.error(exception.getMessage(), exception);
+        }
     }
 }
