4cbe2e0d9ee9f6b3dc00f8ec916ca5e380ae5d04
Yan MATAGNE <ymatagne@norsys.fr>
2017-09-26 13:55:37 -0400

    fix(prod) : add log for second process

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,23 @@
-#FROM openjdk:8-jdk-alpine
 FROM fedora:26
-
+RUN dnf clean packages
 RUN dnf update -y
 RUN dnf -y install dnf-plugins-core
 RUN dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+RUN dnf makecache 
 RUN dnf config-manager --set-enabled docker-ce-edge
 RUN dnf install -y java docker-ce
 RUN curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
 RUN dnf install -y nodejs
 RUN dnf install -y git
 
-RUN useradd -c "pushmyshop" -G root,bin,daemon,sys,adm,disk,wheel,docker pushmyshop
 
+RUN npm set progress=false && npm config set depth 0 && npm cache clean --force
 RUN npm install -g yo --allow-root
 RUN npm uninstall -g https://github.com/pushmyshop/generator-pushmyshop.git
 RUN npm install -g https://github.com/pushmyshop/generator-pushmyshop.git --allow-root
+RUN dnf install -y sudo
 
+RUN adduser --groups docker,wheel,ftp pushmyshop
 USER pushmyshop
 
 RUN mkdir -p /home/pushmyshop/.config/configstore/
--- src/main/java/com/github/pushmyshop/pushmyshopapi/handlers/CompagnyEventHandler.java
+++ src/main/java/com/github/pushmyshop/pushmyshopapi/handlers/CompagnyEventHandler.java
@@ -1,15 +1,14 @@
 package com.github.pushmyshop.pushmyshopapi.handlers;
 
 import com.github.pushmyshop.pushmyshopapi.models.Compagny;
-import com.github.pushmyshop.pushmyshopapi.models.Customer;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.rest.core.annotation.HandleAfterCreate;
-import org.springframework.data.rest.core.annotation.HandleAfterLinkSave;
 import org.springframework.data.rest.core.annotation.HandleAfterSave;
 import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import java.io.*;
+import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 @Component
 @Slf4j
@@ -19,11 +18,33 @@ public class CompagnyEventHandler {
     @HandleAfterSave()
     public void handleAfterSave(Compagny compagny) {
         log.info("[After Save] Create file for generate web app");
-        ProcessBuilder process = new ProcessBuilder("yo", "pushmyshop","/tmp/pushmyshop/",String.format("%s",compagny.getId()), String.format("%s",compagny.getName()));
+        ProcessBuilder processBuilder = new ProcessBuilder("yo", "pushmyshop", "/tmp/pushmyshop/", String.format("%s", compagny.getId()), String.format("%s", compagny.getName()));
         try {
-            process.start();
-              } catch (IOException exception) {
-          log.error(exception.getMessage(), exception);
+            processBuilder.redirectErrorStream(true);
+            processBuilder.directory(new File("/tmp/"));
+
+            Process process = processBuilder.start();
+            StreamGobbler streamGobbler =
+                    new StreamGobbler(process.getInputStream(), System.out::println);
+            Executors.newSingleThreadExecutor().submit(streamGobbler);
+            log.info("generated web site");
+        } catch (IOException exception) {
+            log.error(exception.getMessage(), exception);
+        }
+    }
+    private static class StreamGobbler implements Runnable {
+        private InputStream inputStream;
+        private Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
         }
     }
 }
